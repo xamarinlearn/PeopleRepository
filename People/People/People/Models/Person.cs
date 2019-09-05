a431c8909dcc420a5b0a66834665642309c3b547
jese.alvarado <jese.alvarado@CDS2UNIVO7>
2019-09-05 08:38:57 -0600

    Model class Person added

---
+++ People/People/People/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace People.Models
+{
+    [Table("T_People")]
+  public class Person
+    {
+        // Miembro: Properties
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+      [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
