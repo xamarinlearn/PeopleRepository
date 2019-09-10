886c57da7b3471fb73df2d0e8e5b128203d67dea
jese.alvarado <jese.alvarado@CDS2UNIVO7>
2019-09-10 07:49:19 -0600

    Create M added

--- People/People/People/PersonRepositoryCRUD.cs
+++ People/People/People/PersonRepositoryCRUD.cs
@@ -1,22 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using People.Models;
 using SQLite;
-using People.Models;
 
 namespace People
 {
-  public  class PersonRepositoryCRUD
+    public class PersonRepositoryCRUD
     {
         SQLiteConnection conn;
+
+        // conn = new SQLiteConnection
         public string StatusMessage { get; set; }
 
+        // Constructor
         public PersonRepositoryCRUD(string dbPath)
         {
             // Creamos la conexión.
             conn = new SQLiteConnection(dbPath);
             conn.CreateTable<Person>();
-            
+
+            /* PersonRepositoryCRUD Repo = 
+            new PersonRepositoryCRUD("c:\dev\datos");*/
         }
+
+        // CRUD OPERATIONS o métodos
+        // cREAR
+
+        /* Tipos de datos de retorno:
+         *  void = Vacío
+         *  int = Entero
+         *  string = Cadena
+         *  Decimal = decimal
+         *  Person = un objeto de tipo Peson.
+        */
+        public void CreatePerson(Person newPerson)
+        {
+           
+            int result;
+            result = conn.Insert(newPerson);
+            if (result == 1)
+            {
+                StatusMessage =
+                    $"{result} registro agregado [Nombre: " +
+                    $"{newPerson.Name}, ID:{newPerson.Id}";
+            }
+        }
+
+
+
+
+    }
+
+    class Car
+    {
+        // Catacterísticas o Propiedades
+        public int NPlaca { get; set; }
+        public string Modelo { get; set; }
+
+        // Métodos acciones
+        void Arrancar()
+        {
+            //
+        }
+        void Avanzar()
+        {
+            //
+        }
+
     }
 }
